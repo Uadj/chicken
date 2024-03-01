22cbddc6a8f6079ae75e02103697c2f4f8dac55f
DGW <ds257873@naver.com>
2024-03-02 00:26:51 +0900

    data init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO item (id, item_name, item_price, item_content) VALUES
+                                                               (1, 'Fried Chicken', 15000, 'Crispy and delicious fried chicken'),
+                                                               (2, 'Spicy Chicken', 16000, 'Hot and spicy fried chicken'),
+                                                               (3, 'Garlic Chicken', 16000, 'Flavorful garlic seasoned fried chicken');
